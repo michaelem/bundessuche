69541f597ef7ba60388c19276e74ee83b6163d70
Michael Emhofer <michael@emhofer.net>
2024-01-02 15:02:34 +0100

    Fix migration names

--- db/migrate/20231229205813_add_records_search_index.rb
+++ db/migrate/20231229205813_add_records_search_index.rb
@@ -1,4 +1,4 @@
-class AddRecordSearchIndex < ActiveRecord::Migration[7.1]
+class AddRecordsSearchIndex < ActiveRecord::Migration[7.1]
   def change
     add_index :records,
               "to_tsvector('german', title || ' ' || call_number)",
--- db/migrate/20240101230133_add_unique_index_to_records.rb
+++ db/migrate/20240101230133_add_unique_index_to_records.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexToRecord < ActiveRecord::Migration[7.1]
+class AddUniqueIndexToRecords < ActiveRecord::Migration[7.1]
   def change
     add_index :records, :source_id, unique: true
   end
