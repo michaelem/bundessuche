ddb5d7e2a442e0d5be0ab3a4cdc65f1bda7cf910
Michael Emhofer <michael@emhofer.net>
2024-01-28 19:46:21 +0100

    Highlight search term in results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,4 +3,9 @@ module SearchHelper
     query ||= ""
     Digest::SHA512.hexdigest query + Rails.application.config.cache_key_salt
   end
+
+  def highlight(query, text)
+    return text if query.blank? || text.blank?
+    text.gsub(/(#{query})/i, '<span class="result__highlight">\1</span>').html_safe
+  end
 end
