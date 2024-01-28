c1f5d8b1422420ab04041896518457622338f14a
Michael Emhofer <michael@emhofer.net>
2024-01-28 18:57:25 +0100

    Localize site to german

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module Bundessuche
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.cache_key_salt = Rails.application.credentials.cache_key_salt
+
+    config.i18n.default_locale = :de
   end
 end
