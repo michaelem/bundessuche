a6b63e9705df5c061ab95dda61663e2ee5db2f5f
Michael Emhofer <michael@emhofer.net>
2024-02-18 12:43:36 +0100

    Add benchmark to predict full data import time

---
+++ test/benchmarks/bundesarchiv_importer_benchmark.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+require "benchmark"
+
+class BundesarchivImporterBenchmark < ActiveSupport::TestCase
+  # Run benchmarks in sequence:
+  parallelize(workers: 1, threshold: 1)
+
+  def test_reduced_dataset
+    assert_equal 0, Record.count
+
+    time =
+      Benchmark.measure do
+        BundesarchivImporter.new("test/fixtures/files/dataset-2-percent").run()
+      end
+
+    Record.delete_all
+
+    puts "Reduced dataset import: #{time.to_s}"
+    puts "Predicted time for full dataset: #{format("%.d", time.real * 50 / 60)} minutes"
+    pass("Reduced dataset import: #{time.to_s}")
+  end
+end
--- test/importers/bundesarchiv_importer_test.rb
+++ test/importers/bundesarchiv_importer_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 class BundesarchivImporterTest < ActiveSupport::TestCase
   test "importer creates right amount of Records from test files" do
     assert_equal 0, Record.count
-    BundesarchivImporter.new("test/fixtures/files").run
+    BundesarchivImporter.new("test/fixtures/files/dataset-tiny").run
     assert_equal 794, Record.count
   end
 
   # DE-1958_ed3ff8a0-c65e-4efd-b5d3-96950687d291 is part of DE-1958_B_153.xml in the fixture files
   test "importer extracts the correct date for DE-1958_ed3ff8a0-c65e-4efd-b5d3-96950687d291" do
-    BundesarchivImporter.new("test/fixtures/files").run
+    BundesarchivImporter.new("test/fixtures/files/dataset-tiny").run
 
     record =
       Record.find_by(source_id: "DE-1958_ed3ff8a0-c65e-4efd-b5d3-96950687d291")
