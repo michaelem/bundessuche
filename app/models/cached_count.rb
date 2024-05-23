ed5f94e71f388c0981eea56e1326c166ccd9fe8a
Michael Emhofer <michael@emhofer.net>
2024-05-23 12:50:45 +0200

    Add schema annotations in models

--- app/models/cached_count.rb
+++ app/models/cached_count.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: cached_counts
+#
+#  id         :integer          not null, primary key
+#  count      :bigint
+#  model      :string
+#  scope      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_cached_counts_on_model_and_scope  (model,scope) UNIQUE
+#
 class CachedCount < ApplicationRecord
 end
--- app/models/origination.rb
+++ app/models/origination.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: originations
+#
+#  origin_id :bigint
+#  record_id :bigint
+#
+# Indexes
+#
+#  index_originations_on_origin_id                (origin_id)
+#  index_originations_on_record_id                (record_id)
+#  index_originations_on_record_id_and_origin_id  (record_id,origin_id) UNIQUE
+#
 class Origination < ApplicationRecord
   belongs_to :record
   belongs_to :origin
