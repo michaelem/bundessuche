4641c05612ca805a3b0f9f8e0f5fa7ab7bbf8c76
Michael Emhofer <michael@emhofer.net>
2024-07-25 00:22:41 +0200

    Fix issues with punctuation in queries
    https://stackoverflow.com/questions/2027527/sqlite-virtual-table-match-escape-character

--- app/models/record_trigram.rb
+++ app/models/record_trigram.rb
@@ -18,10 +18,10 @@ class RecordTrigram < ApplicationRecord
         ->(query) do
           return none if query.blank?
 
-          where(record_trigrams: "title: #{query}")
-            .or(where(record_trigrams: "summary: #{query}"))
-            .or(where(record_trigrams: "parents: #{query}"))
-            .or(where(record_trigrams: "origin_names: #{query}"))
+          where(record_trigrams: "title: \"#{query}\"")
+            .or(where(record_trigrams: "summary: \"#{query}\""))
+            .or(where(record_trigrams: "parents: \"#{query}\""))
+            .or(where(record_trigrams: "origin_names: \"#{query}\""))
             .order(:call_number)
         end
 end
