49d4f8988a789a41783061a67ddb8b15017ede1f
Michael Emhofer <michael@emhofer.net>
2024-08-26 21:49:49 +0200

    Pick out specific file for testing instead of relying on order

--- test/models/archive_file_trigram_test.rb
+++ test/models/archive_file_trigram_test.rb
@@ -5,7 +5,7 @@ class ArchiveFileTrigramTest < ActiveSupport::TestCase
     BundesarchivImporter.new("test/fixtures/files/dataset-tiny").run
     ArchiveFile.reindex
 
-    @example_archive_file = ArchiveFile.last
+    @example_archive_file = ArchiveFile.find_by(source_id: "DE-1958_8a0ff3f6-46b3-443a-9e48-946e790301e0")
   end
 
   test "search finds archive files by title" do
