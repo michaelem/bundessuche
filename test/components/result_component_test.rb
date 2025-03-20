cfbb02e40e9415b3e0b0bb26a8dbecfe9c2ca5fb
Michael Emhofer <michael@emhofer.net>
2025-03-20 23:31:42 +0100

    Replace OpenStruct with Minitest::Mock
    Since OpenStruct was removed from the standard library.

--- test/components/result_component_test.rb
+++ test/components/result_component_test.rb
@@ -1,32 +1,49 @@
 require "test_helper"
 
 class ResultComponentTest < ViewComponent::TestCase
+  setup do
+    @archive_file = Minitest::Mock.new
+  end
+
   def test_title_highlight
+    @archive_file.expect(:title, "Rechenzentrum Duisburg")
+
     component =
       ResultComponent.new(
         query: "rechenzentrum",
-        archive_file: OpenStruct.new(title: "Rechenzentrum Duisburg")
+        archive_file: @archive_file
       )
+
     assert_equal %(<span class="result__highlight">Rechenzentrum</span> Duisburg),
                  component.title
+    @archive_file.verify
   end
 
   def test_date
+    @archive_file.expect(:source_date_text, "1989")
+    @archive_file.expect(:source_date_text, "1989")
+
     component =
       ResultComponent.new(
         query: "rechenzentrum",
-        archive_file: OpenStruct.new(source_date_text: "1989")
+        archive_file: @archive_file
       )
+
     assert_equal "1989", component.date
+    @archive_file.verify
   end
 
   def test_summary_highlight
+    @archive_file.expect(:summary, "Das Rechenzentrum in Duisburg")
+
     component =
       ResultComponent.new(
         query: "rechenzentrum",
-        archive_file: OpenStruct.new(summary: "Das Rechenzentrum in Duisburg")
+        archive_file: @archive_file
       )
+
     assert_equal %(Das <span class="result__highlight">Rechenzentrum</span> in Duisburg),
                  component.summary
+    @archive_file.verify
   end
 end
