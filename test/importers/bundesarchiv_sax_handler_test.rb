23c0b52e0200a77ab67da5b137ea38b28c996caa
Michael Emhofer <michael@emhofer.net>
2026-05-17 22:48:39 +0200

    Add a simple empty test case for the SAX Handler

---
+++ test/importers/bundesarchiv_sax_handler_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class BundesarchivSaxHandlerTest < ActiveSupport::TestCase
+  test "skips archdesc with type other than inventory" do
+    xml = <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <ead>
+        <archdesc type="other">
+          <c level="file" id="should-be-skipped">
+            <did>
+              <unittitle>Should not be imported</unittitle>
+            </did>
+          </c>
+        </archdesc>
+      </ead>
+    XML
+
+    handler = BundesarchivSaxHandler.new({})
+    Nokogiri::XML::SAX::Parser.new(handler).parse(xml)
+
+    assert_equal 0, handler.total_count
+    assert_equal 0, ArchiveFile.count
+  end
+end
